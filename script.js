33bd20d216e49ba70d1812056dbc458dcdef6862
Olatoyosi <khadeejah005@gmail.com>
2015-03-16 10:45:07 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	$('#btnClick').click(function(){
+	if($('#months').val()==='january'){
+		$( '#display').empty();
+		$('#display').append("<p>Yes, you are the coolest of them all...buh you dont act it.<br/>.Motivate yourself and kick some butt</p>");
+	 }
+	 if($('#months').val()==='february'){
+	 	$( '#display').empty();
+
+		$('#display').append("I see love all over you....love everywhere.<br />....so wipe away that smirk and love someone in return");
+	 }
+	 if($('#months').val()==='march'){
+	 	$( '#display').empty();
+		$('#display').append("<p>Everyone knows you are the best. <br />They are all trying to keep pace with your<br/>...pay attention to them</p>");
+	 }
+	 if($('#months').val()==='april'){
+	 	$( '#display').empty();
+		$('#display').append("<p>No matter how many fortune telling you try...its all the same.<br/>GET OUT OF BED AND DO SOME WORK!!</p>");
+	}
+		if($('#months').val()==='may'){
+		$( '#display').empty();
+
+		$('#display').append("<p>You are the type i would call a finger in every pie...<br />But you keep forgetting the love pie.<br />Today is the day to fall in love</p>");
+	 }
+	 if($('#months').val()==='june'){
+	 	$( '#display').empty();
+
+		$('#display').append("<p>Hmm june, june, june....just talk less and do more.<br />Thats what you need to shut the world up</p>");
+	 }
+	 if($('#months').val()==='july'){
+	 	$( '#display').empty();
+
+		$('#display').append("<p>Just try and be less complicated ok...<br />And take cofee...it helps with the moods</p>");
+	 }
+	 if($('#months').val()==='august'){
+	 	$( '#display').empty();
+
+		$('#display').append("<p>The only thing not special about you is what i havent found out<br />This world is your stepping stone</p>");
+	}
+		if($('#months').val()==='september'){
+			$( '#display').empty();
+
+		$('#display').append("<p>Hmm you are a kinda person that should always be on the move. no stopping no resting, just keep moving</p>");
+	}
+		if($('#months').val()==='october'){
+			$( '#display').empty();
+		$('#display').append("<p>you need a makeover in every aspect of your life.<br />Start now!!!!</p>");
+	 }
+	 if($('#months').val()==='november'){
+	 	$( '#display').empty();
+
+		$('#display').append("<p>Achiever, Over achiever, Super Over achiever<br />Give yourself a break!!</p>");
+	 }
+	 if($('#months').val()==='december'){
+	 	$( '#display').empty();
+
+		$('#display').append("<p>Common shake out of that mood..<br />Seems you are lacking a lot of energy.<br />I know just what you need..A KISS MUAHHHH</p>");
+	 }
+
+
+});
+
+});
+
+
+
+
